bee27fea222fb9fbfcacc96e6927e42b65f77301
Doug Wright <mrdougwright@gmail.com>
2013-06-21 00:44:41 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Salus::Application.config.secret_token = '145ca5468dc95d341a71dbdc996b11e5a8d337da7c4e98fa589e8100e9b11c3a3b3bb65f3a15338bbc952547ae381e01af74a3b0ca1c7a8a8a2d27b7712da69d'
